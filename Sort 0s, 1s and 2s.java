6a89fb58534ba541db0e56d611ac103e18e05314
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2025-02-08 00:14:44 +0530

    Create Sort 0s, 1s and 2s.java

---
+++ Sort 0s, 1s and 2s.java
@@ -0,0 +1,86 @@
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+You need to solve this problem without utilizing the built-in sort function.
+
+Examples:
+
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+ //{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] inputArray = input.split("\\s+");
+            int a[] = new int[inputArray.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
+
+            Solution ob = new Solution();
+            ob.sort012(a);
+
+            for (int num : a) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        // code here
+        int n = arr.length;
+        int zero = 0,one=0,two=0;
+        for(int i=0;i<n;i++) {
+            if(arr[i]==0) {
+                zero++;
+            }
+            else if(arr[i]==1) {
+                one++;
+            }
+            else if(arr[i]==2) {
+                two++;
+            }
+        }
+        int j=0;
+        while(zero > 0) {
+            arr[j] = 0;
+            zero--;
+            j++;
+        }
+        while(one > 0) {
+            arr[j] = 1;
+            one--;
+            j++;
+        }
+        while(two > 0) {
+            arr[j] = 2;
+            two--;
+            j++;
+        }
+    }
+}
+
+Accuracy 100%
+
+//{ Driver Code Starts.
+// } Driver Code Ends
